23d9ae9b5325df091fa648209e0b42925799ae7c
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-17 15:36:12 +0100

    test that player has access to a board

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,4 +4,10 @@ describe Player do
 
   it { is_expected.to respond_to(:board) }
 
+  describe '#board' do
+    it 'should be able to return a grid' do
+      expect(subject.board.row_size).to eq(10)
+    end 
+  end
+
 end
